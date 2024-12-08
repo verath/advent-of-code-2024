d76f37b553b5e8b128cd1bec8aa5e166d2c56200
Peter Eliasson <warackta@gmail.com>
2024-12-08 09:50:52 +0100

    day 2+3

---
+++ day02/day02.py
@@ -0,0 +1,80 @@
+from typing import Generator
+import unittest
+from pathlib import Path
+
+TEST_INPUT = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+""".strip()
+
+SCRIPT_DIR = Path(__file__).parent
+INPUT = (SCRIPT_DIR / "input.txt").read_text().strip()
+
+
+def check_safe(levels: list[int]) -> bool:
+    expect_increasing = levels[1] > levels[0]
+    for i in range(1, len(levels)):
+        diff = levels[i] - levels[i - 1]
+        abs_diff = abs(diff)
+        is_increasing = diff > 0
+        if abs_diff < 1 or abs_diff > 3:
+            return False
+        if (expect_increasing and not is_increasing) or (
+            not expect_increasing and is_increasing
+        ):
+            return False
+    return True
+
+
+def read_levels(input: str) -> Generator[list[int], None, None]:
+    for line in input.splitlines():
+        yield [int(c) for c in line.split(" ")]
+
+
+def part1(input: str) -> int:
+    num_safe = 0
+    for levels in read_levels(input):
+        num_safe += 1 if check_safe(levels) else 0
+    return num_safe
+
+
+def part2(input: str) -> int:
+    num_safe = 0
+    for levels in read_levels(input):
+        is_safe = check_safe(levels)
+        if not is_safe:
+            for i in range(len(levels)):
+                levels_copy = levels.copy()
+                levels_copy.pop(i)
+                is_safe = check_safe(levels_copy)
+                if is_safe:
+                    break
+        num_safe += 1 if is_safe else 0
+    return num_safe
+
+
+class Day02Test(unittest.TestCase):
+
+    def test_is_safe(self) -> None:
+        self.assertTrue(check_safe([7, 6, 4, 2, 1]))
+        self.assertFalse(check_safe([1, 2, 7, 8, 9]))
+        self.assertFalse(check_safe([9, 7, 6, 2, 1]))
+        self.assertFalse(check_safe([1, 3, 2, 4, 5]))
+        self.assertFalse(check_safe([8, 6, 4, 4, 1]))
+        self.assertTrue(check_safe([1, 3, 6, 7, 9]))
+
+    def test_part1(self) -> None:
+        self.assertEqual(part1(TEST_INPUT), 2)
+        self.assertEqual(part1(INPUT), 660)
+
+    def test_part2(self) -> None:
+        self.assertEqual(part2(TEST_INPUT), 4)
+        self.assertEqual(part2(INPUT), 689)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ day03/day03.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass
+from typing import Generator, TypeAlias
+import unittest
+from pathlib import Path
+import re
+
+TEST_INPUT_PART1 = (
+    "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+)
+TEST_INPUT_PART2 = (
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+)
+
+SCRIPT_DIR = Path(__file__).parent
+INPUT = (SCRIPT_DIR / "input.txt").read_text().strip()
+
+
+@dataclass
+class Mul:
+    lhs: int
+    rhs: int
+
+
+@dataclass
+class Do:
+    pass
+
+
+@dataclass
+class Dont:
+    pass
+
+
+Instruction: TypeAlias = Mul | Do | Dont
+
+MUL_PATTERN = re.compile(r"mul\((?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})\)")
+DO_PATTERN = re.compile(r"do\(\)")
+DONT_PATTERN = re.compile(r"don't\(\)")
+
+
+def parse_instructions(s: str) -> Generator[Instruction, None, None]:
+    while s:
+        if m := MUL_PATTERN.match(s):
+            s = s[m.end() :]
+            lhs = int(m.group("lhs"))
+            rhs = int(m.group("rhs"))
+            yield Mul(lhs, rhs)
+        elif m := DO_PATTERN.match(s):
+            s = s[m.end() :]
+            yield Do()
+        elif m := DONT_PATTERN.match(s):
+            s = s[m.end() :]
+            yield Dont()
+        else:
+            s = s[1:]
+
+
+def part1(input: str) -> int:
+    return sum(
+        inst.lhs * inst.rhs
+        for inst in parse_instructions(input)
+        if isinstance(inst, Mul)
+    )
+
+
+def part2(input: str) -> int:
+    sum = 0
+    enabled = True
+    for instruction in parse_instructions(input):
+        match instruction:
+            case Do():
+                enabled = True
+            case Dont():
+                enabled = False
+            case Mul(lhs, rhs) if enabled:
+                sum += lhs * rhs
+
+    return sum
+
+
+class Day03Test(unittest.TestCase):
+    def test_part1(self) -> None:
+        self.assertEqual(part1(TEST_INPUT_PART1), 161)
+        self.assertEqual(part1(INPUT), 174960292)
+
+    def test_part2(self) -> None:
+        self.assertEqual(part2(TEST_INPUT_PART2), 48)
+        self.assertEqual(part2(INPUT), 56275602)
+
+
+if __name__ == "__main__":
+    unittest.main()
